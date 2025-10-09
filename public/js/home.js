0aacaeb7a3dc657ec6265f6d78f18d209b62afef
muhammad-islam-dean <kalebkandake@gmail.com>
2025-10-09 11:36:17 -0400

    v1

---
+++ public/js/home.js
@@ -0,0 +1,19 @@
+const dropdowns = []
+const droplinks = []
+document.querySelectorAll('.dropdown').forEach(i => dropdowns.push(i))
+document.querySelectorAll('.droplink').forEach(i => droplinks.push(i))
+let current = -67
+function toggleDropdown(i) {
+    if (current === i) {
+        dropdowns[i].classList.toggle('hidden')
+        return 
+    }
+    if (current > -1) dropdowns[current].classList.add('hidden')
+    current = i
+    dropdowns[i].classList.remove('hidden')
+    
+}
+for (let i = 0; i < dropdowns.length; i++) {
+    dropdowns[i].classList.toggle('hidden')
+    droplinks[i].addEventListener('click', () => toggleDropdown(i))
+}
